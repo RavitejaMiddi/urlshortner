6dc10a42760a25ea95c745130147ceeafb47c29e
RaviTejaMiddi <medhi.ravi@gmail.com>
2025-08-11 22:58:09 -0500

    controller added

--- src/main/java/com/urlshortner/controllers/UrlShortnerController.java
+++ src/main/java/com/urlshortner/controllers/UrlShortnerController.java
@@ -1,12 +1,19 @@
 package com.urlshortner.controllers;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("urlshortner/")
+@RequestMapping("/urlshortner")
 public class UrlShortnerController {
 
+
+
+    @GetMapping("/shorten")
+    public String  shortenUrl(String url){
+        return "shortened url";
+    }
 
 }
